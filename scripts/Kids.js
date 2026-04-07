7564f73f85433e36fc966cd99ca40b2c3f088936
Lake Carter <lakecarter8@gmail.com>
2026-04-06 21:34:25 -0500

    working on click event

--- scripts/Kids.js
+++ scripts/Kids.js
@@ -1,15 +1,28 @@
-import { getChildren } from "./database.js"
+import { getChildren } from "./database.js";
 
-const children = getChildren()
+const children = getChildren();
 
 export const Kids = () => {
-    let html = "<ol>"
+  let html = "<ol>";
 
+  for (const child of children) {
+    html += `<li data-id="${child.id}" data-type="child" data-wish="${child.wish}">${child.name}</li>`;
+  }
+
+  html += "</ol>";
+  return html;
+};
+
+//Kid Paring click event
+document.addEventListener("click", (clickEvent) => {
+  const clicked = clickEvent.target;
+  if (clicked.dataset.type === "child") {
+    let clickedKid = null;
     for (const child of children) {
-        html += `<li data-id="${child.id}" data-type="child" data-wish="${child.wish}">${child.name}</li>`
+      if (parseInt(clicked.dataset.id) === child.id) {
+        clickedKid = child;
+        debugger;
+      }
     }
-
-    html += "</ol>"
-    return html
-}
-
+  }
+});
